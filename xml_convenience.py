d05f0df3a1868176b1cfe485e1eb28c2ec2740f6
Mike Raineri <michael.raineri@dell.com>
2023-04-25 11:02:54 -0400

    Updated branch references to 'main'
    Signed-off-by: Mike Raineri <michael.raineri@dell.com>

--- xml_convenience.py
+++ xml_convenience.py
@@ -1,6 +1,6 @@
 # Copyright Notice:
 # Copyright 2017-2020 DMTF. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Schema-Creator/blob/master/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Schema-Creator/blob/main/LICENSE.md
 
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
